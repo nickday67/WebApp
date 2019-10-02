51b9f376fd2903d2ecef97c30e879efb07b74351
Nick Day <thenickday@icloud.com>
2019-10-01 20:53:40 -0400

    updating

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 function startQuiz() {
-    $('#start').on('click', function(event){
-      renderTheQuestion();
-    }
-    );
+  $('#start').on('click', function(event){
+    renderTheQuestion();
   }
+  );
+}
+
+function updateQuestionsAndScore() {
+  const html = $(`<ul>
+      <li id="js-answered">Questions Number: ${STORE.currentQuestion + 1}/${STORE.questions.length}</li>
+      <li id="js-score">Score: ${STORE.score}/${STORE.questions.length}</li>
+    </ul>`);
+  $(".question-and-score").html(html);
+}
+
+function updateOptions() {
+  let question = STORE.questions[STORE.currentQuestion];
+  for(let i=0; i<question.options.length; i++) {
+    $('.js-options').append(`
+        <input type = "radio" name="options" id="option${i+1}" value= "${question.options[i]}" tabindex ="${i+1}"> 
+        <label for="option${i+1}"> ${question.options[i]}</label> <br/>
+        <span id="js-r${i+1}"></span>
+    `);
+  }
+}
